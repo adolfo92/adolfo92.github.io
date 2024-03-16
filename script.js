cf94501864513d0634d461a7329100ff474f5f25
Adolfo <adolfo92dev@gmail.com>
2024-03-16 02:06:15 -0400

    Add content text as an array per paragraph

---
+++ script.js
@@ -0,0 +1,9 @@
+// Content
+
+let aboutMeContent = [
+  "I'm Adolfo Castro, currently living in Caracas, Venezuela. I'm doing web development and many other computer related stuff.",
+  "Currently i'm studying Electrical Engineering at Universidad Central de Venezuela with focus on telecomunications and getting a TSU (Bachelor sort of degree) on Informatic engineering at UNETI.",
+  "I enjoy doing Linux ricing (Desktop enviroment modifications), and automating tasks using linux BASH CLI.",
+  "My current focus on my programming career is mainly on backend develpment and data analysis with python but i can also do web development using many technologies like vanilla JS with bootstrap and/or Wordpress, and Nodejs environment techs like React and Express",
+  "I'm also aviable to learn any other technologies that might be of use like Golang, Rust, etc.",
+];
